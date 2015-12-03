30f6d91b55ee305c53c7671211ce6a80fa0b795e
Josh Taylor <joshrtay@gmail.com>
2015-12-02 23:31:14 -0800

    FIRST

---
+++ Readme.md
@@ -0,0 +1,51 @@
+
+# map-gen
+
+[![Build status][travis-image]][travis-url]
+[![Git tag][git-image]][git-url]
+[![NPM version][npm-image]][npm-url]
+[![Code style][standard-image]][standard-url]
+
+Map for generator.
+
+## Installation
+
+    $ npm install @micro-js/map-gen
+
+## Usage
+
+```js
+var map = require('@micro-js/map-gen')
+
+Array.from(map(addOne, function * () {
+  yield 1
+  yield 2
+  yield 3
+})) // [2, 3, 4]
+
+function addOne(v) {
+  return v + 1
+}
+```
+
+## API
+
+### mapGen(fn, gen)
+
+- `fn` - mapping function
+- `gen` - generates values to map
+
+**Returns:** generator of mapped values
+
+## License
+
+MIT
+
+[travis-image]: https://img.shields.io/travis/micro-js/map-gen.svg?style=flat-square
+[travis-url]: https://travis-ci.org/micro-js/map-gen
+[git-image]: https://img.shields.io/github/tag/micro-js/map-gen.svg
+[git-url]: https://github.com/micro-js/map-gen
+[standard-image]: https://img.shields.io/badge/code%20style-standard-brightgreen.svg?style=flat
+[standard-url]: https://github.com/feross/standard
+[npm-image]: https://img.shields.io/npm/v/@micro-js/map-gen.svg?style=flat-square
+[npm-url]: https://npmjs.org/package/@micro-js/map-gen
---
+++ test/index.js
@@ -0,0 +1,123 @@
+/**
+ * Imports
+ */
+
+var map = require('..')
+var test = require('tape')
+var identity = require('@micro-js/identity')
+
+/**
+ * Tests
+ */
+
+test('should handle no yield', (t) => {
+  var it = map(identity, justReturn)()
+  var next = it.next()
+  t.equal(next.value, 'foo')
+  t.equal(next.done, true)
+  t.end()
+
+  function * justReturn () {
+    return 'foo'
+  }
+
+})
+
+test('should handle only yield', (t) => {
+  var it = map(identity, justYield)()
+  var next = it.next()
+  t.equal(next.value, 'foo')
+  t.equal(next.done, false)
+  next = it.next()
+  t.equal(next.value, undefined)
+  t.equal(next.done, true)
+  t.end()
+
+  function * justYield () {
+    yield 'foo'
+  }
+})
+
+test('should yield input values', (t) => {
+  var it = map(identity, yieldIn)('in')
+
+  t.equal(it.next().value, 'in')
+  t.equal(it.next('foo').value, 'foo')
+
+  var next = it.next('bar')
+  t.equal(next.value, 'bar')
+  t.equal(next.done, true)
+
+  t.end()
+
+  function * yieldIn(input) {
+    var res = yield input
+    res = yield res
+    return res
+  }
+})
+
+test('should yield mapped values', (t) => {
+  var it = map(add, yieldIn)(0)
+
+  t.equal(it.next().value, 1)
+  t.equal(it.next(2).value, 3)
+
+  var next = it.next(10)
+  t.equal(next.value, 10)
+  t.equal(next.done, true)
+
+  t.end()
+
+  function add (v) {
+    return v + 1
+  }
+
+  function * yieldIn(input) {
+    var res = yield input
+    res = yield res
+    return res
+  }
+})
+
+test('should catch errors', (t) => {
+  var it = map(identity, error)()
+  var next = it.next()
+  t.equal(next.value, 'foo')
+  next = it.throw(new Error('test'))
+  t.equal(next.value, 'bar')
+  t.end()
+  function * error () {
+    try {
+      yield 'foo'
+    } catch (e) {
+      yield 'bar'
+    }
+  }
+})
+
+test('should catch error in mapF', (t) => {
+  var it = map(tError, error)()
+  var next = it.next()
+  t.equal(next.value, 'bar')
+  next = it.next()
+  t.equal(next.value, undefined)
+  t.equal(next.done, true)
+  t.end()
+
+  function tError (v) {
+    if (v === 'foo') {
+      throw new Error('test')
+    } else {
+      return v
+    }
+  }
+
+  function * error () {
+    try {
+      yield 'foo'
+    } catch (e) {
+      yield 'bar'
+    }
+  }
+})
